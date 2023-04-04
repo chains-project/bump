d49aee628f5a0d2909e9106a8d8eba42031a0b00
Gabriel Skoglund <gabrielskoglund@gmail.com>
2023-04-05 01:49:26 +0200

    Rewrite CLI to be easier to use
    Using the BreakingUpdateMiner application now starts with selecting one of the commands 'mine' or 'find', which makes the CLI a bit cleaner to use.

--- src/main/java/miner/Main.java
+++ src/main/java/miner/Main.java
@@ -1,6 +1,6 @@
 package miner;
 
-import org.jetbrains.annotations.NotNull;
+import miner.GitHubMiner.RepositorySearchConfig;
 import picocli.CommandLine;
 
 import java.io.IOException;
@@ -16,106 +16,109 @@ import java.util.List;
  */
 public class Main {
 
-    /** The minimum number of stars required when searching for GitHub repositories */
-    private static final int MINIMUM_NUMBER_OF_STARS = 10;
-
     public static void main(String[] args) {
-        CommandLineParams params = getCommandLineParams(args);
-
-        try {
-            List<String> apiTokens = Files.readAllLines(params.tokens);
-            GitHubMiner miner = new GitHubMiner(apiTokens, params.outputDirectory);
-            if (params.searchType.reposToMine != null) {
-                miner.mineRepositories(params.searchType.reposToMine);
-            } else {
-                System.out.println("Attempting to find repos");
-                Path foundRepos = params.searchType.foundRepos;
-                if (foundRepos == null) {
-                    Path filePath = params.outputDirectory.resolve(GitHubMiner.FOUND_REPOS_FILE);
-                    try {
-                        foundRepos = Files.writeString(filePath, JsonUtils.EMPTY_JSON_OBJECT,
-                                                       StandardOpenOption.CREATE_NEW);
-                    } catch (IOException e) {
-                        System.err.println("Could not create a output file in " + filePath +
-                            " as the file might already exist. Either specify a different output path or provide" +
-                            " an explicit output file for found repositories.");
-                        System.exit(1);
-                    }
-                    System.out.println("Created a new file for found repos as " + filePath);
-                }
-                miner.findRepositories(new RepositoryList(foundRepos), MINIMUM_NUMBER_OF_STARS);
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
+        int exitCode = new CommandLine(new CLIEnryPoint()).execute(args);
+        System.exit(exitCode);
     }
 
-    @NotNull
-    private static CommandLineParams getCommandLineParams(String[] args) {
-        CommandLineParams params = new CommandLineParams();
-        CommandLine commandLine = new CommandLine(params);
-        try {
-            commandLine.parseArgs(args);
-        } catch (CommandLine.MissingParameterException e) {
-            System.out.println(e.getMessage());
-            commandLine.usage(System.out);
-            System.exit(1);
-        }
-        if (commandLine.isUsageHelpRequested() || args.length == 0) {
-            System.exit(0);
-        }
-        return params;
+    @CommandLine.Command(subcommands = {Mine.class, Find.class},mixinStandardHelpOptions = true,version = "0.1")
+    public static class CLIEnryPoint implements Runnable {
+        @Override
+        public void run() { CommandLine.usage(this, System.out); }
     }
 
-    private static class CommandLineParams {
+    @CommandLine.Command(name = "mine", mixinStandardHelpOptions = true, version = "0.1")
+    private static class Mine implements Runnable {
         @CommandLine.Option(
-            names = {"-a", "--api-tokens"},
-            paramLabel = "TOKEN-FILE",
-            description = "A file containing a newline separated list of GitHub API tokens",
-            required = true
+                names = {"-a", "--api-tokens"},
+                paramLabel = "TOKEN-FILE",
+                description = "A file containing a newline separated list of GitHub API tokens",
+                required = true
         )
-        Path tokens;
+        Path apiTokenFile;
 
         @CommandLine.Option(
-            names = {"-o", "--output-directory"},
-            paramLabel = "OUTPUT-DIR",
-            description = "A directory where output data will be stored.",
-            required = true
+                names = {"-o", "--output-directory"},
+                paramLabel = "OUTPUT-DIR",
+                description = "A directory where output data will be stored.",
+                required = true
         )
         Path outputDirectory;
 
         @CommandLine.Option(
-            names = {"-h", "--help"},
-            description = "Print this help message.",
-            usageHelp = true
+                names = {"-r", "--repos"},
+                paramLabel = "REPO-FILE",
+                description = "A JSON file as given by the find-repos operation.",
+                required = true
         )
-        boolean usageHelpRequested;
+        Path repoFile;
 
-        @CommandLine.ArgGroup(multiplicity = "1")
-        SearchType searchType;
+        @Override
+        public void run() {
+            try {
+                List<String> apiTokens = Files.readAllLines(apiTokenFile);
+                new GitHubMiner(apiTokens, outputDirectory).mineRepositories(repoFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
-        static class SearchType {
-            @CommandLine.Option(
-                names = {"-m", "--mine"},
-                arity = "1", // A file is required
+    @CommandLine.Command(name = "find", mixinStandardHelpOptions = true, version = "0.1")
+    private static class Find implements Runnable {
+        @CommandLine.Option(
+                names = {"-a", "--api-tokens"},
+                paramLabel = "TOKEN-FILE",
+                description = "A file containing a newline separated list of GitHub API tokens",
+                required = true
+        )
+        Path apiTokenFile;
+
+        @CommandLine.Option(
+                names = {"-o", "--output-directory"},
+                paramLabel = "OUTPUT-DIR",
+                description = "A directory where output data will be stored.",
+                required = true
+        )
+        Path outputDirectory;
+        @CommandLine.Option(
+                names = {"-s", "--search-config"},
+                paramLabel = "SEARCH-CONFIG",
+                description = "A JSON file specifying details about the repositories to search for.",
+                required = true
+        )
+        Path searchConfigFile;
+
+        @CommandLine.Option(
+                names = {"-r", "--repos"},
                 paramLabel = "REPO-FILE",
-                description = "Search the given repositories for matching pull requests. The file provided should " +
-                              "be a JSON file as given by the --discover-repos operation."
-            )
-            Path reposToMine;
+                description = "A JSON file containing previously found repositories. If used, this file will be updated," +
+                        "otherwise a new file called found_repositories.json will be created in the output directory."
+        )
+        Path repoFile;
 
-            @CommandLine.Option(
-                names = {"-f", "--find-repos"},
-                arity = "0..1",
-                paramLabel = "REPO-FILE",
-                fallbackValue = CommandLine.Option.NULL_VALUE,
-                description = "Search GitHub for repos matching the requirements." +
-                              "If a file is provided, found repositories will be added to this file," +
-                              "otherwise a file named found_repositories.json will be created in the output directory" +
-                              "and used to store the results."
-            )
-            Path foundRepos;
+        @Override
+        public void run() {
+            if (repoFile == null) {
+                Path filePath = outputDirectory.resolve(GitHubMiner.FOUND_REPOS_FILE);
+                try {
+                    repoFile = Files.writeString(filePath, JsonUtils.EMPTY_JSON_OBJECT, StandardOpenOption.CREATE_NEW);
+                } catch (IOException e) {
+                    System.err.println("Could not create a output file in " + filePath +
+                            " as the file might already exist. Either specify a different output path or provide" +
+                            " an explicit output file for found repositories.");
+                    System.exit(1);
+                }
+                System.out.println("Created a new file for found repos as " + filePath);
+            }
+            try {
+                List<String> apiTokens = Files.readAllLines(apiTokenFile);
+                RepositorySearchConfig searchConfig = RepositorySearchConfig.fromJson(searchConfigFile);
+                var repoList = new RepositoryList(repoFile);
+                new GitHubMiner(apiTokens, outputDirectory).findRepositories(repoList, searchConfig);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
