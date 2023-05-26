16a6b2446da27a9547a73ca90908b7e40a948a7c
YogyaGamage <yogyagamage@gmail.com>
2023-05-26 15:09:42 +0200

    Add the option to reproduce all existing breaking updates

--- src/main/java/reproducer/Main.java
+++ src/main/java/reproducer/Main.java
@@ -4,6 +4,7 @@ import miner.BreakingUpdate;
 import miner.JsonUtils;
 import picocli.CommandLine;
 
+import java.io.File;
 import java.nio.file.Path;
 
 /**
@@ -51,8 +52,7 @@ public class Main {
         @CommandLine.Option(
                 names = {"-f", "--file"},
                 paramLabel = "BREAKING-UPDATE-FILE",
-                description = "A JSON file describing a breaking update.",
-                required = true
+                description = "A JSON file describing a breaking update."
         )
         Path breakingUpdateFile;
 
@@ -60,8 +60,13 @@ public class Main {
         public void run() {
             ResultManager resultManager = new ResultManager(datasetDir, reproductionDir, jarDir);
             BreakingUpdateReproducer reproducer = new BreakingUpdateReproducer(resultManager);
-            BreakingUpdate bu = JsonUtils.readFromFile(breakingUpdateFile, BreakingUpdate.class);
-            reproducer.reproduce(bu);
+            if (breakingUpdateFile != null) {
+                BreakingUpdate bu = JsonUtils.readFromFile(breakingUpdateFile, BreakingUpdate.class);
+                reproducer.reproduce(bu);
+            } else {
+                File[] breakingUpdates = datasetDir.toFile().listFiles();
+                reproducer.reproduceAll(breakingUpdates);
+            }
         }
     }
 }
