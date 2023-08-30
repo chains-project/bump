4e39c288a854e386f821751ea1c1a6d254c73c31
Yogya Tulip Gamage <47789154+yogyagamage@users.noreply.github.com>
2023-08-30 08:40:30 +0200

    Update benchmark by adding GitHub org and new failure categories (#126)

--- src/main/java/miner/GitHubMiner.java
+++ src/main/java/miner/GitHubMiner.java
@@ -15,7 +15,6 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
